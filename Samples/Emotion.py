dc4429bcfc694f71409aabbc3a6cdf4cc9ce0289
mildock <youngwook@outlook.com>
2016-07-28 12:22:11 +0900

    Bug fix

--- Samples/Emotion.py
+++ Samples/Emotion.py
@@ -4,18 +4,15 @@ import http.client, urllib.request, urllib.parse, urllib.error, base64
 headers = {
     # Request headers
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '02669de042544b1e8a2bdfccfbf30a4f',
+    'Ocp-Apim-Subscription-Key': '1e6a0214e8e243f69825e35b19270bbc',
 }
 
 params = urllib.parse.urlencode({
-    # Request parameters
-    'visualFeatures': 'Categories',
-    'details': '',
 })
 
 try:
     conn = http.client.HTTPSConnection('api.projectoxford.ai')
-    conn.request("POST", "/vision/v1.0/analyze?%s" % params, "{url:'http://postfiles6.naver.net/20160530_117/warit_1464561113138dr7yL_JPEG/IMG_1094.jpg?type=w3'}", headers)
+    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, "{url:'http://postfiles6.naver.net/20160530_117/warit_1464561113138dr7yL_JPEG/IMG_1094.jpg?type=w3'}", headers)
     response = conn.getresponse()
     data = response.read()
     print(data)
